910b36a018b8a35f3d4464e3c63b3feedfd5cc03
Sam Kirby <sam.kirby94@hotmail.co.uk>
2019-05-07 05:24:34 +0100

    Commit mod files

---
+++ src/main/java/uk/bobbytables/arcfurnacepatch/core/ArcFurnacePatchTransformer.java
@@ -0,0 +1,57 @@
+package uk.bobbytables.arcfurnacepatch.core;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
+
+import java.util.Iterator;
+
+import static uk.bobbytables.arcfurnacepatch.core.ArcFurnacePatchLoadingPlugin.AFP_LOG;
+import static uk.bobbytables.arcfurnacepatch.core.ArcFurnacePatchLoadingPlugin.UPDATE;
+
+public class ArcFurnacePatchTransformer implements IClassTransformer {
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] basicClass) {
+        if (!transformedName.equals("blusunrize.immersiveengineering.common.blocks.metal.TileEntityArcFurnace"))
+            return basicClass;
+    
+        ClassReader classReader = new ClassReader(basicClass);
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        
+        ClassNode classNode = new ClassNode();
+        classReader.accept(classNode, 0);
+
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals(UPDATE)) {
+                Iterator<AbstractInsnNode> insnNodeIterator = methodNode.instructions.iterator();
+                while (insnNodeIterator.hasNext()) {
+                    AbstractInsnNode abstractInsnNode = insnNodeIterator.next();
+                    if (abstractInsnNode.getOpcode() == Opcodes.INVOKEVIRTUAL) {
+                        MethodInsnNode methodInsnNode = (MethodInsnNode)abstractInsnNode;
+                        if (methodInsnNode.name.equals("setInputAmounts")) {
+                            int invokeIndex = methodNode.instructions.indexOf(abstractInsnNode);
+                            int targetIndex = invokeIndex - 26;
+                            AbstractInsnNode targetInsnNode = methodNode.instructions.get(targetIndex);
+                            if (targetInsnNode.getOpcode() != Opcodes.ICONST_1) {
+                                AFP_LOG.warn("Not applying arc furnace patch, opcode mismatch! Has this been resolved in Immersive Engineering?");
+                            }
+                            InsnList insnList = new InsnList();
+                            insnList.add(new VarInsnNode(Opcodes.ALOAD, 5));
+                            insnList.add(new FieldInsnNode(Opcodes.GETFIELD, "blusunrize/immersiveengineering/api/crafting/ArcFurnaceRecipe", "input", "Lblusunrize/immersiveengineering/api/crafting/IngredientStack;"));
+                            insnList.add(new FieldInsnNode(Opcodes.GETFIELD, "blusunrize/immersiveengineering/api/crafting/IngredientStack", "inputSize", "I"));
+                            methodNode.instructions.insertBefore(targetInsnNode, insnList);
+                            methodNode.instructions.remove(targetInsnNode);
+                            AFP_LOG.info("Successful transformation of {}", transformedName);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+}
